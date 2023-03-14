62f9420c104b0bf22f8497ed484b2ca6f5ff1218
Hyunghoon Kim <hyunghki@cx2r4s4.42seoul.kr>
2023-03-14 14:26:26 +0900

    [docs] some change boj21609

--- 김형훈/week2/2023.03.07/boj21609/21609.cpp
+++ 김형훈/week2/2023.03.07/boj21609/21609.cpp
@@ -19,6 +19,74 @@ struct block
 	block(int num) : num(num), area(0){};
 };
 
+block find_group()
+{
+	block max_group(-1);
+	bool vis[22][22] = {0};
+	queue<pair<int, int>> q;
+	for (int i = N - 1; i >= 0; i--)
+		for (int j = N - 1; j >= 0; j--)
+			if (board[i][j] > 0 && !vis[i][j])
+			{
+				vis[i][j] = true;
+				q.push({i, j});
+				block target(board[i][j]);
+				target.coor.push({i, j});
+				target.num = board[i][j];
+				while (!q.empty())
+				{
+					auto [x, y] = q.front();
+					q.pop();
+					for (int dir = 0; dir < 4; dir++)
+					{
+						int nx = x + "1012"[dir] - '1';
+						int ny = y + "0121"[dir] - '1';
+						if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+							continue;
+						if (vis[nx][ny] || board[nx][ny] == -1)
+							continue;
+						if (board[nx][ny] == 0)
+							target.rainbow.push_back({nx, ny});
+						else
+						{
+							if (target.num != board[nx][ny])
+								continue;
+							target.coor.push({nx, ny});
+						}
+						vis[nx][ny] = true;
+						q.push({nx, ny});
+					}
+				}
+				target.area = target.coor.size() + target.rainbow.size();
+				target.standard = target.coor.top();
+				if (target.area >= 2)
+				{
+					if (target.area > max_group.area)
+						max_group = target;
+					else if (target.area == max_group.area)
+					{
+						if (target.rainbow.size() > max_group.rainbow.size())
+							max_group = target;
+						else if (target.rainbow.size() == max_group.rainbow.size())
+						{
+							if (target.standard.first > max_group.standard.first)
+								max_group = target;
+							else if (target.standard.first == max_group.standard.first)
+								if (target.standard.second > max_group.standard.second)
+									max_group = target;
+						}
+					}
+				}
+				// 무지개 블록은 탐색을 다시 진행해야 하기 때문에 vis를 false로 바꿔줌.
+				for (auto e : target.rainbow)
+				{
+					auto [x, y] = e;
+					vis[x][y] = false;
+				}
+			}
+	return (max_group);
+}
+
 void gravity()
 {
 	for (int j = 0; j < N; j++)
@@ -59,69 +127,7 @@ int main()
 	int ans = 0;
 	while (1)
 	{
-		block max_group(-1);
-		bool vis[22][22] = {0};
-		queue<pair<int, int>> q;
-		for (int i = N - 1; i >= 0; i--)
-			for (int j = N - 1; j >= 0; j--)
-				if (board[i][j] > 0 && !vis[i][j])
-				{
-					vis[i][j] = true;
-					q.push({i, j});
-					block target(board[i][j]);
-					target.coor.push({i, j});
-					target.num = board[i][j];
-					while (!q.empty())
-					{
-						auto [x, y] = q.front();
-						q.pop();
-						for (int dir = 0; dir < 4; dir++)
-						{
-							int nx = x + "1012"[dir] - '1';
-							int ny = y + "0121"[dir] - '1';
-							if (nx < 0 || ny < 0 || nx >= N || ny >= N)
-								continue;
-							if (vis[nx][ny] || board[nx][ny] == -1)
-								continue;
-							if (board[nx][ny] == 0)
-								target.rainbow.push_back({nx, ny});
-							else
-							{
-								if (target.num != board[nx][ny])
-									continue;
-								target.coor.push({nx, ny});
-							}
-							vis[nx][ny] = true;
-							q.push({nx, ny});
-						}
-					}
-					target.area = target.coor.size() + target.rainbow.size();
-					target.standard = target.coor.top();
-					if (target.area >= 2)
-					{
-						if (target.area > max_group.area)
-							max_group = target;
-						else if (target.area == max_group.area)
-						{
-							if (target.rainbow.size() > max_group.rainbow.size())
-								max_group = target;
-							else if (target.rainbow.size() == max_group.rainbow.size())
-							{
-								if (target.standard.first > max_group.standard.first)
-									max_group = target;
-								else if (target.standard.first == max_group.standard.first)
-									if (target.standard.second > max_group.standard.second)
-										max_group = target;
-							}
-						}
-					}
-					// 무지개 블록은 탐색을 다시 진행해야 하기 때문에 vis를 false로 바꿔줌.
-					for (auto e : target.rainbow)
-					{
-						auto [x, y] = e;
-						vis[x][y] = false;
-					}
-				}
+		block max_group = find_group();
 		// 그룹이 존재하지 않으면 탐색 종료
 		if (max_group.num == -1)
 			break;

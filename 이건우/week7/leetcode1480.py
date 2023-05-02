93549e9e268411b2a3f188bb6c460090bf1e2dab
Lee Keon Woo <lkeonw0o@naver.com>
2023-05-02 20:14:37 +0900

    Update 이건우/week7/leetcode1480.py
    Co-authored-by: MinJae Kim <33440010+minjae9610@users.noreply.github.com>

--- 이건우/week7/leetcode1480.py
+++ 이건우/week7/leetcode1480.py
@@ -1,12 +1,3 @@
 class Solution(object):
     def runningSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        res = []
-        sums = 0
-        for _ in nums:
-            sums += _
-            res.append(sums)
-        return res
+        return [sum(nums[:i+1]) for i in range(len(nums))]
